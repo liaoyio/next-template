60ca1adb3ec361b48e85b3868b6ace1c77208049
liaoyi <2417276459@qq.com>
2023-12-18 16:20:50 +0800

    feat: - :sparkles:

--- src/app/(动态路由)/blog/page.tsx
+++ src/app/(动态路由)/blog/page.tsx
@@ -8,7 +8,10 @@ export default function Page() {
           <a href="/blog/1">Blog 1 🔖</a>
           <a href="/blog/2">测试 Blog error 页面 ❌</a>
           <a href="/blog/3">Blog 3 🔖</a>
-          <a href="/blog/4">测试 Blog 404 页面 ➡️</a>
+          <a href="/blog/4">
+            测试 Blog 404 页面 ⚠️：动态路由添加layout，无法添加 not-found 页面，build 后会报错
+          </a>
+          <a href="/blog/5">Blog 5 ➡️</a>
           <a href="/blog/5">Blog 5 🙅</a>
           <a href="/blog/6">Blog 6 🙅</a>
         </div>
